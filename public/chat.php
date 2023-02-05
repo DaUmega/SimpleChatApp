8246645542075c0edc10c92a3af5a61356fa538f
Tony <daumega@gmail.com>
2023-02-05 17:15:58 -0500

    Bare bone working version, still need to tweak http and API requests to utilize framework more

--- public/chat.php
+++ public/chat.php
@@ -2,14 +2,18 @@
 require __DIR__ . '/../myconfig/dbconnect.php';
 
 session_start();
-if (isset($_GET["Id"]))
-$_SESSION["Id"] = $_GET["Id"];
 $user = $pdo->query("SELECT * FROM Users WHERE Id = ".$_SESSION["Id"]." ")->fetch();
-if (isset($_POST["msg"])) {
-	$cmd = "INSERT INTO Msgs(Message) VALUES('" . $_POST["msg"] . "')";
+if (isset($_GET["Id"])) {
+	$_SESSION["Id"] = $_GET["Id"];
+}
+if (isset($_GET["ToId"])) {
+	$_SESSION["ToId"] = $_GET["ToId"];
+}
+if (isset($_POST["message"])) {
+	$cmd = "INSERT INTO Msgs(FromId, ToId, Message) VALUES('".$_SESSION["Id"]."', '".$_SESSION["ToId"]."' , '" . $_POST["message"] . "')";
 
 	if ($pdo->query($cmd))
-		header('Location: chat.php/ToId=' . $_SESSION["Id"] );
+		header('Location: chat.php/ToId=' . $_SESSION["ToId"] );
 	else
 		echo "Error with chat.";
 }
@@ -38,29 +42,27 @@ if (isset($_POST["msg"])) {
 				}
 			?>
 		</ul>
-		<?php if (isset($_GET["ToId"])) : ?>
+		<?php if (isset($_SESSION["ToId"])) : ?>
 			<p>Message History:</p>
 			<?php
-				$stmt = $pdo->query("SELECT * FROM Msgs WHERE (FromId = ".$_SESSION["Id"]." AND ToId = ".$_GET["ToId"].") OR 
-				(FromId = ".$_GET["ToId"]." AND ToId = ".$_SESSION["Id"].") ")->fetchAll();
+				$stmt = $pdo->query("SELECT * FROM Msgs WHERE (FromId = ".$_SESSION["Id"]." AND ToId = ".$_SESSION["ToId"].") OR 
+				(FromId = ".$_SESSION["ToId"]." AND ToId = ".$_SESSION["Id"].") ")->fetchAll();
 				foreach ($stmt as $row) {
 					$fromstmt = $pdo->query("SELECT * FROM Users WHERE Id = ".$row["FromId"]." ")->fetch();
 					$fromname = $fromstmt["Name"];
 					$tostmt = $pdo->query("SELECT * FROM Users WHERE Id = ".$row["ToId"]." ")->fetch();
 					$toname = $tostmt["Name"];
 					$msg = $row["Message"];
-					echo "FROM: ", $fromname;
-					echo "Message: ", $msg;
-					echo "TO: ", $toname;
+					echo "FROM: ", $fromname, " TO: ", $toname, "<br>";
+					echo "Message: ", $msg, "<br>";
 				}
-				echo 'chat.php?ToId=' . $_GET["ToID"] . ' ';
-				echo '<form action = "chat.php?ToId='.$_GET["ToID"].'" method = "POST">
-					<p>Type your message here:</p>
-					<input type = "text" message = "msg" FromId = '.$_SESSION["Id"].'" ToId = "'.$_GET["ToID"].'" multiple/>
-					<br>
-					<input type = "submit" name = "submit" class = "btn btn-primary" value = "Submit"/>
-					</form>';
 			?>
+			<form action = "chat.php" method = "POST">
+				<p>Type your message here:</p>
+				<textarea name = "message"></textarea>
+				<br>
+				<input type = "submit" name = "submit" class = "btn btn-primary" value = "Submit"/>
+			</form>
 		<?php endif; ?>
 		<a href="/">Home</a>
 	</div>
